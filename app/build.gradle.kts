45b9563288cc892ce952f3bf79cd9c7c58815184
Hassan Ali <hassanaliielsayed@gmail.com>
2025-05-27 14:47:44 +0300

    Changing CompileSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.khizana_user"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.khizana_user"
