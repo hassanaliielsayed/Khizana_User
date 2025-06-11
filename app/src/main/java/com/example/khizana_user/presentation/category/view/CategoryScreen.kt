5ca3c4cad6e7652910f2ee943cb1d4426c460dfe
ayaahmed2058 <ayaahmedmohamed85@gmail.com>
2025-06-11 08:48:53 +0300

    edit category screen design

--- app/src/main/java/com/example/khizana_user/presentation/category/view/CategoryScreen.kt
+++ app/src/main/java/com/example/khizana_user/presentation/category/view/CategoryScreen.kt
@@ -53,7 +53,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
