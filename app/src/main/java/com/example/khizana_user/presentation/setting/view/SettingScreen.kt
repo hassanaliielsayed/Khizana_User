206df74da17a79fa00c76838beb3ea20302e818c
ayaahmed2058 <ayaahmedmohamed85@gmail.com>
2025-06-18 01:48:07 +0300

    edit contact us screen design

--- app/src/main/java/com/example/khizana_user/presentation/setting/view/SettingScreen.kt
+++ app/src/main/java/com/example/khizana_user/presentation/setting/view/SettingScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material3.AlertDialog
@@ -36,6 +37,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
@@ -89,7 +91,7 @@ fun SettingScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background)
+                .background(colorResource(id = R.color.light_background))
                 .padding(horizontal = 16.dp)
                 .padding(innerPadding)
         ) {
@@ -108,10 +110,14 @@ fun SettingScreen(
             )
 
             Card(
-                modifier = Modifier.fillMaxWidth(),
-                shape = MaterialTheme.shapes.medium,
-                elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
-                colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp),
+                elevation = CardDefaults.cardElevation(4.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.White
+                ),
+                shape = RoundedCornerShape(12.dp),
             ) {
                 Column(modifier = Modifier.padding(vertical = 8.dp)) {
                     SettingItem(
@@ -122,8 +128,8 @@ fun SettingScreen(
 
                     Divider(
                         modifier = Modifier.padding(horizontal = 16.dp),
-                        color = MaterialTheme.colorScheme.outline.copy(alpha = 0.1f),
-                        thickness = 1.dp
+                        color = colorResource(R.color.dark_blue),
+                        thickness = 2.dp
                     )
 
                     SettingItem(
@@ -134,8 +140,8 @@ fun SettingScreen(
 
                     Divider(
                         modifier = Modifier.padding(horizontal = 16.dp),
-                        color = MaterialTheme.colorScheme.outline.copy(alpha = 0.1f),
-                        thickness = 1.dp
+                        color = colorResource(R.color.dark_blue),
+                        thickness = 2.dp
                     )
 
                     SettingItem(
@@ -145,8 +151,8 @@ fun SettingScreen(
 
                     Divider(
                         modifier = Modifier.padding(horizontal = 16.dp),
-                        color = MaterialTheme.colorScheme.outline.copy(alpha = 0.1f),
-                        thickness = 1.dp
+                        color = colorResource(R.color.dark_blue),
+                        thickness = 2.dp
                     )
 
                     SettingItem(
